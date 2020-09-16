9ff362a585d8dbdcc6fad646f93efc0760b6394e
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-16 15:01:58 -0500

    Fix space errors 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Print a message
- *
- * Return: Value 0
+* main - Print message
+* Return: 0
 */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
