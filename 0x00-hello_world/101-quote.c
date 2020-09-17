96f81be6aba248ea1ce0d3acaad9a835378ac1ef
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-16 23:22:07 -0500

    Fix command error 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - print a quote
-*Return - 0
+* main - Print a quote
+* Return: 0
 */
 
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
