5366331cdad510f791f9bd9d4d27c376ba4c5efd
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-17 14:19:44 -0500

    Add 2-print_alphabet.c script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lower case.
+* Return: 0
+*/
+
+int main(void)
+{
+char letra;
+
+for (letra = 'a' ; letra <= 'z' ; letra++)
+{
+putchar(letra);
+putchar(10);
+}
+
+return (0);
+}
