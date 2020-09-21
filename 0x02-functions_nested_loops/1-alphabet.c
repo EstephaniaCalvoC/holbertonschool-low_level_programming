26552b29134b72ee016aaab8d162d5b5af0fc580
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-21 08:03:50 -0500

    Add 1-alphabet.c script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <holberton.h>
+
+/**
+ * print_alphabet - Print the alphabet, in lowercase, followed by new line.
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 ### About it
 
 - **0-holberton.c:** Print Holberton, followed by a new line.
+- **1-alphabet.c:** Function that prints the alphabet, in lowercase, followed by a new line.
