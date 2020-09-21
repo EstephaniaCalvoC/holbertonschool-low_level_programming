9c507af297cc10fce4ff02d9038b6b5c8ae95b60
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-21 07:50:49 -0500

    Add 0-holberton.c script

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char name[9];
+	int i;
+
+	name = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(name[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
