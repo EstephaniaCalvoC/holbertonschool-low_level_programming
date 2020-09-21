8bf98357eb119bec63de178dade1defc67b55675
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-21 08:15:22 -0500

    Add 2-print_alphabet_x10.c function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Print 10 times the alphabet, in lowercase.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 
 - **0-holberton.c:** Print Holberton, followed by a new line.
 - **1-alphabet.c:** Function that prints the alphabet, in lowercase, followed by a new line.
+- **2-print_alphabet_x10.c:** Print 10 times the alphabet, in lowercase, followed by a new line.
