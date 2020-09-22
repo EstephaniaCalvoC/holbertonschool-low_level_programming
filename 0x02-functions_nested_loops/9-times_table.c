a60f0fdd68e120b795821f7c9639c5b474d52bc2
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-22 09:44:14 -0500

    Add 9-times_table.c function

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * times_table - description
+ */
+
+void times_table(void)
+{
+	int num1, num2, r;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 2; num2 < 10; num2++)
+		{
+			r = num1 * num2;
+
+			if (r < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + r);
+			}
+			else
+			{
+				_putchar('0' + (r / 10));
+				_putchar('0' + (r % 10));
+			}
+
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		_putchar('\n');
+	}
+}
