8b86ce9d0825c1773f7065513fdff2a55404a760
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 10:04:06 -0500

    Add 4-print_most_numbers.c function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print the numbers, from 0 to 9 except 2 and 4.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '0'; i++)
+	{
+		if (i == '2' && i == '4')
+			continue;
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,4 @@
 - **1-isdigit.c:** Check for a digit (0 through 9).
 - **2-mul.c:** Multiplie two integers.
 - **3-print_numbers.c:** Print the numbers, from 0 to 9, followed by a new line.
+- **4-print_most_numbers.c:** Print the numbers, from 0 to 9, followed by a new line.
