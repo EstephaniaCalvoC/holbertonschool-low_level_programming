256681e43b877e3edd1ed5c77a1e7404795cc3f9
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 14:06:58 -0500

    Add 7-print_diagonal.c function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Print a diagonal in the terminal.
+ * @n: numbers of lines.
+ */
+
+void print_diagonal(int n)
+{
+	int spc, init = n;
+
+	for (; n > 0; n--)
+	{
+		for (spc = init - n; spc > 0; spc--)
+			_putchar(' ');
+		_putchar('\\');
+	}
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -9,3 +9,4 @@
 - **4-print_most_numbers.c:** Print the numbers, from 0 to 9, followed by a new line.
 - **5-more_numbers.c:** Prints 10 times the numbers, from 0 to 14.
 - **6-print_line.c:** Draw a straight line in the terminal.
+- **7-print_diagonal.c:** Draw a diagonal line on the terminal.
