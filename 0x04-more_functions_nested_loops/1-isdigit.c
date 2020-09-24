1e282f6cf2a59db91151e49c62e30a3e2b01e92a
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 08:51:35 -0500

    Add 1-isdigit.c function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isadigit - Check for a digit (0 through 9).
+ * @c: Charater to check
+ * Return: 1 if c is a digit, return 0 otherwise.
+ */
+
+int _isadigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 ### About it
 
 - **0-isupper.c:** Check for uppercase character.
+- **1-isdigit.c:** Check for a digit (0 through 9).
