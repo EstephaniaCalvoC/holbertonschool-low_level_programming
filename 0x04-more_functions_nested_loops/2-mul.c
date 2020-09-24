a7510e504a2deb37a90c72326acdeab7430af520
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 09:02:48 -0500

    Add 2-mul.c function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplie two integers
+ * @a: Number 1
+ * @b: Number 2
+ * Return: The result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 
 - **0-isupper.c:** Check for uppercase character.
 - **1-isdigit.c:** Check for a digit (0 through 9).
+- **2-mul.c:** Multiplie two integers.
