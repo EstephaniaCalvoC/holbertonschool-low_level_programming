7bf99c43137af19fb9a6023886075a9ded0a3961
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 17:22:51 -0500

    Add 10-print_triangle.c function

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Print a triangle in the terminal.
+ * @n: numbers of lines.
+ */
+
+void print_triangle(int size)
+{
+	int spc, i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+		for (spc = size -i; spc > 0; spc--)
+			_putchar(' ');
+		for (j = i; j > 0; j--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
