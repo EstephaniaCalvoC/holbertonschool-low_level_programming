ea0560019f25e7f9d1af038339b020d31559da7b
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 15:37:37 -0500

    Fix error 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,15 @@
 
 void print_square(int size)
 {
-	int rows = size;
+	int rows, columns;
 
-	for (; rows > 0; rows--)
+	if (size <= 0)
+		_putchar('\n');
+
+	for (rows = size; rows > 0; rows--)
 	{
-		for (; size > 0; size--)
-			_putchar('\#');
+		for (columns = size ; columns > 0; columns--)
+			_putchar('#');
 		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
