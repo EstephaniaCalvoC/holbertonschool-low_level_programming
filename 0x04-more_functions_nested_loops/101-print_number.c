285e299588528243512fe8dab4e0b0ecfd7a4992
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-24 23:53:01 -0500

    Recursive form 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,15 +10,16 @@ void print_number(int n)
 {
 	unsigned int nx;
 
-	if (n < 0)
+	nx = n
+
+	if (nx < 0)
 	{
 		_putchar(45);
-		n = n * -1;
+		nx = nx * -1;
 	}
-	nx = n;
 
-	if (n < 10)
-		_putchar('0' + n);
+	if (nx < 10)
+		_putchar('0' + nx);
 	else
 	{
 		print_number(nx / 10);
