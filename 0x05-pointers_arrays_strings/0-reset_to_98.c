7625a890a574dc055908c4ea556e724be523bd93
EstephaniaCalvoC <2177@holbertonschool.com>
2020-09-28 06:42:30 -0500

    Add 0-reset_to_98.c function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Take a pointer to an int as parameter.
+ * @*n: pointer to a integer number.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 ## README
 
 ### About it
+
+-**0-reset_to_98.c:** Take a pointer to an int as parameter and updates the value it points to to 98.
