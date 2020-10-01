e72022124f140354643b3c7732f30ac675a5d594
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-01 16:29:33 -0500

    Fix error 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int i, tmp;
 
-	for (i = 0; i < n; i++, n--)
+	for (i = 0; i <= n; i++, n--)
 	{
 		tmp = a[i];
 		a[i] = a[n - 1];
-		a[n] = tmp;
+		a[n - 1] = tmp;
 	}
 }
