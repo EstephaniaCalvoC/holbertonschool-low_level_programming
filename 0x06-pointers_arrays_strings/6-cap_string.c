9f835e9ab1b5f9346a8723ef2a514360407e45fa
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-01 20:36:00 -0500

    Fix error 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,9 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[j] <= 'z')
+		{
 			s[i] = s[i] - 32;
+		}
 		for (j = 0; sep[j] != '\0'; j++)
 		{
 			if (s[i] == sep[j]
@@ -26,8 +28,6 @@ char *cap_string(char *s)
 				s[i + 1] = s[i + 1] - 32;
 			}
 		}
-
 	}
-
 	return (s);
 }
