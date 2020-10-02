b359e34acc301330e2ff0a5b1ea9d82560588823
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-01 20:06:22 -0500

    Add 6-cap_string.c function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalize all words of a string.
+ * @s: string
+ * Return: Capitalize string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if ((s[i] == sep[j]
+			     && s[i + 1] != '0'
+			     && s[i + 1] >= 'a'
+			     && s[i + 1] <= 'z')
+			    || i == 0)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -7,3 +7,5 @@
 - **2-strncpy.c:** Copy n bytes from source to destiny string.
 - **3-strcmp.c:** Compare two strings.
 - **4-rev_array.c:** Reverse the content of an array of integers.
+- **5-string_toupper.c:** Change all lowercase letters of a string to uppercase.
+- **6-cap_string.c:** Capitalize all words of a string.
