0bb314019a634270f3fd5dd51f1505852bc371bd
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-01 21:00:57 -0500

    Add 7-leet function

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * leet - Encode a string into 1337
+ * @c: char to check
+ * Return: 0 is success
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char l[] = "aeotlAEOTL";
+	char n[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; l[j] != '\0'; j++)
+			if (s[i] == l[j])
+				s[i] = n[j];
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -9,3 +9,4 @@
 - **4-rev_array.c:** Reverse the content of an array of integers.
 - **5-string_toupper.c:** Change all lowercase letters of a string to uppercase.
 - **6-cap_string.c:** Capitalize all words of a string.
+- **7-leet.c:** Encode a string into 1337.
