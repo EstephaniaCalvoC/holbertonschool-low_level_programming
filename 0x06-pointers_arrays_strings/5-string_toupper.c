7e4244eaf7845c93c4982226e460ec24f1397ab4
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-02 22:24:03 -0500

    Add 5-string_toupper.c function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Change all lowercase letters of a string to uppercase.
+ * @s: string
+ * Return: Upper string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
