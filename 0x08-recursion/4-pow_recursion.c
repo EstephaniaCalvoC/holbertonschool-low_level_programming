9fbd3b055bc40388f99c11386d6bcf540c283449
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-07 15:14:03 -0500

    Fix 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,17 +2,23 @@
 
 /**
  * _pow - Calculate x raised to the power of y.
- * @x: Number x.
- * @y: Number y.
+ * @x: Base number.
+ * @y: Power.
  * Return: Result of the pow.
  */
 
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
+	{
 		return (-1);
+	}
 	else if (y == 0)
+	{
 		return (1);
+	}
 	else
+	{
 		return (x * _pow_recursion(x, y - 1));
+	}
 }
