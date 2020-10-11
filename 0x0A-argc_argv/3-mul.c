7c32e5b9d8f06fef6fb90ddec156fba24fa91982
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-10 20:08:20 -0500

    Fix import error in 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <sdlib.h>
 
 /**
  * main - Multiplie two numbers.
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		puts("Error");
+		printf("Error\n");
 		return (1);
 	}
 	if (argc > 0)
