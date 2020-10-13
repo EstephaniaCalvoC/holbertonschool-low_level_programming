9e01f1c0ead4c03f3e188f7b8395a6506c5febf4
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-13 17:42:46 -0500

    Add 3-alloc_grid.c function

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,40 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * alloc_grid -
- *
- * Return: 
+ * alloc_grid - Return a pointer to a 2 dimensional array of integers.
+ * @width: Columns.
+ * @height: Rows.
+ * Return: Pointer to a 2 dimensional array.
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
 
+	int **arrd2;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	/*Create a pointer to pointer rows*/
+	arrd2 = (int **) malloc(height * sizeof(int *));
+
+	if (arrd2 == NULL)
+		return (NULL);
+
+	/*For each row create a pointer to each element*/
+	for (i = 0; i < height; i++)
+	{
+		arrd2[i] = (int *)malloc(width * sizeof(int));
+		if (arrd2[i] == NULL)
+			return (NULL);
+	}
+
+	/*Fill the array*/
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arrd2[i][j] = 0;
+	}
+
+	return (arrd2);
 }
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -27,11 +27,11 @@ What I learned from this project:
 ### [2. He who is not courageous enough to take risks will accomplish nothing in life](./2-str_concat.c)
 * Write a function that concatenates two strings.
 
-<!--
+
 ### [3. If you even dream of beating me you'd better wake up and apologize](./3-alloc_grid.c)
 * Write a function that returns a pointer to a 2 dimensional array of integers.
 
-
+<!--
 ### [4. It's not bragging if you can back it up](./4-free_grid.c)
 * Write a function that frees a 2 dimensional grid previously created by your alloc_grid function.
 
