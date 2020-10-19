44df44aba3baa2dfd3ce27c27b7ea3ab6498d5ca
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-19 15:41:35 -0500

    Fix type error 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,12 @@
  * @age: Dog's age.
  * @owner: Dog's owner.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	/*The structure dog have a name, age, owner elements*/
+
 	if (d)
 	{
-		/*The structure dog have a name, age, owner elements*/
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
