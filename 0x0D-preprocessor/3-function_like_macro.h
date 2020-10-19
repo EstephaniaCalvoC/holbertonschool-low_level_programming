7390eba779b379956692a3692ecb27d1da350f2b
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-19 12:47:26 -0500

    Fix type 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((X) < 0 ? (X * -1) : (X))
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H */
+#endif
