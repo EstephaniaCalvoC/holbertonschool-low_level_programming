d4ff27370b6268df613142a4e1957020fea38878
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-19 16:12:53 -0500

    Fix error 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,7 @@ void print_dog(struct dog *d)
 		else
 			printf("(nil)\n");
 
-		if (d->age != NULL)
-			printf("Age: %f\n"   d->age);
-		else
-			printf("(nil)\n");
+		printf("Age: %f\n", d->age);
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
