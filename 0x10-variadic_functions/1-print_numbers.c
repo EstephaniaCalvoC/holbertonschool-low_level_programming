6734e5442fd14bc892ac0ffe315cd91927179dcd
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-23 11:53:08 -0500

    Add 1-print_numbers.c function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,27 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_numbers -
- *
- * Return: 
+ * print_numbers - Print numbers, followed by a new line.
+ * @separator: String to be printed between numbers.
+ * @n: Number of integers.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list ap;
+	unsigned int i;
 
+	va_start(ap, n);
+
+	if (separator == NULL)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (n - i - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
 }
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -21,11 +21,11 @@ What I learned from this project:
 ### [0. Beauty is variable, ugliness is constant](./0-sum_them_all.c)
 * A function that returns the sum of all its parameters.
 
-<!---
+
 ### [1. To be is to be the value of a variable](./1-print_numbers.c)
 * A function that prints numbers, followed by a new line.
 
-
+<!---
 ### [2. One woman's constant is another woman's variable](./2-print_strings.c)
 * A function that prints strings, followed by a new line.
 
