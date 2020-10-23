3c6ab2af49179f9ad246e54b07d39781c0be7a53
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-23 11:28:29 -0500

    Fix betty style and include library error 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * sum_them_all - Calculate the sum of all its parameters
- *
+ * sum_them_all - Calculate the sum of all its parameters.
+ * @n: Number of arguments.
  * Return: The result.
  */
 int sum_them_all(const unsigned int n, ...)
@@ -11,10 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int result = 0;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
-		result += va_arg (ap, int);
+		result += va_arg(ap, int);
 
 	va_end(ap);
 	return (result);
