14185044b0cf6ac621faa5e39ddec000d5ca5a13
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-24 22:16:08 -0500

    Fix error 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,50 @@
 #include <stdio.h>
 
 /**
+ * print_all - Print anything
+ * @format: A list of types of arguments passed to the function.
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	int nf;/* Number of formats*/
+	va_list element;
+	format_t arr_f[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL},
+	};
+	char *separator = "";
+
+	va_start(element, format);
+
+	/*Run each character of format*/
+	nf = 0;
+
+	while (format != NULL && format[nf] != '\0')
+	{
+		/*Print if the character is valid*/
+		i = 0;
+		while (i < 4)
+		{
+			if (*arr_f[i].f == format[nf])
+			{
+				printf("%s", separator);
+				arr_f[i].print(element);
+				separator = ", ";
+				break;
+			}
+			i++;
+		}
+		nf++;
+	}
+	printf("\n");
+	va_end(element);
+}
+
+/**
  * print_c - Print with character format.
  * @element: List of elements
  */
@@ -40,51 +84,4 @@ void print_s(va_list element)
 	if (str == NULL)
 		str = "(nil)";
 	printf("%s", str);
-}
-
-/**
- * print_all - Print anything
- * @format: A list of types of arguments passed to the function.
- */
-void print_all(const char * const format, ...)
-{
-	int i;
-	va_list element;
-	unsigned int nf;/* Number of formats*/
-	format_t arr_f[] = {
-		{'c', print_c},
-		{'i', print_i},
-		{'f', print_f},
-		{'s', print_s},
-	};
-	char *separator;
-
-	if (format == NULL)
-		return;
-
-	separator = "";
-
-	va_start(element, format);
-
-	/*Run each character of format*/
-	nf = 0;
-
-	while (format[nf])
-	{
-		/*Print if the character is valid*/
-		i = 0;
-		while (i < 4)
-		{
-			if (arr_f[i].f == format[nf])
-			{
-				printf("%s", separator);
-				arr_f[i].print(element);
-				separator = ", ";
-			}
-			i++;
-		}
-		nf++;
-	}
-	printf("\n");
-	va_end(element);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 
 void print_c(va_list element);
 void print_i(va_list element);
@@ -18,10 +16,11 @@ void print_s(va_list element);
  */
 typedef struct format_t
 {
-	char f;
+	char *f;
 	void (*print)(va_list);
 } format_t;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
