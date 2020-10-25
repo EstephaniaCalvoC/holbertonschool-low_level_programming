ca17f60f852ab323106cad73d9f6f3d3b580b5c8
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-24 21:24:27 -0500

    Fix separator detection order 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++9)
 	{
 		printf("%d", va_arg(ap, int));
-		if ((n - i - 1) && separator)
+		if (i != (n - 1) && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
