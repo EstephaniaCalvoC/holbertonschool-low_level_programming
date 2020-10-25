20301ebf7545969eb1b72fade03c6ff260787a4f
EstephaniaCalvoC <2177@holbertonschool.com>
2020-10-24 21:49:21 -0500

    Fix requirement errors 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,14 +11,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *s;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char *));
-		if (n - i - 1)
-			printf("%s", separator);
+		s = va_arg(ap, char *);
+
+		if (!s)
+			printf("(nil)");
+		else
+		{
+			printf("%s", s);
+			if (i < (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(ap);
