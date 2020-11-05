73918c9cb23bf1d4ec0630c1a4bd56d6ac1f29c5
EstephaniaCalvoC <2177@holbertonschool.com>
2020-11-05 21:06:43 +0000

    Add 1-print_binary.c function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
-
 /**
- * print_binary -
- *
- * Return: 
+ * prt_binary_rec - Prints the binary representation of a number.
+ * @n: Number to print.
+ */
+void prt_binary_rec(unsigned long int n)
+{
+	short int bn; /*Binary number (bit)*/
+
+	if (n != 0)
+	{
+		/*Enter the function by removing the last bit*/
+		prt_binary_rec(n >> 1);
+		bn = n & 1;
+		_putchar(bn + '0');
+	}
+}
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: Numbert to print.
  */
 void print_binary(unsigned long int n)
 {
-
+	if (n != 0)
+		prt_binary_rec(n);
+	else
+		_putchar('0');
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -16,11 +16,10 @@ What I learned from this project:
 ### [0. 0](./0-binary_to_uint.c)
 * A function that converts a binary number to an unsigned int.
 
-<!---
 ### [1. 1](./1-print_binary.c)
 * A function that prints the binary representation of a number.
 
-
+<!---
 ### [2. 10](./2-get_bit.c)
 * A function that returns the value of a bit at a given index.
 
