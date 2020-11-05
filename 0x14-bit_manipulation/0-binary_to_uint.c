be45ad6abb0195118389115b2582505eebfecce8
EstephaniaCalvoC <2177@holbertonschool.com>
2020-11-05 20:12:05 +0000

    Add 0-binary_to_uint.c function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,49 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint -
- *
- * Return: 
+ * _strlen - Count the length of a string.
+ * @s: String.
+ * Return: Length.
+ */
+int _strlen(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c]; c++)
+		;
+
+	return (c);
+}
+
+
+/**
+ * binary_to_uint - Convert a binary number to an unsigned int.
+ * @b: Pointer to a string of 0 and 1 chars.
+ * Return: The converted number, or 0 if.
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int idx;/*Index* of b*/
+	unsigned int num = 0;/*Decminal number*/
+	unsigned int k = 1;/*Powers of two*/
 
+	if (b == NULL)
+		return (0);
+
+	/*Maximum index*/
+	idx = _strlen(b) - 1;
+
+	/*Run from the last to the first*/
+	for (; idx >= 0; idx--)
+	{
+		if (b[idx] != '0' && b[idx] != '1')
+			return (0);
+
+		if (b[idx] == '1')
+			num += k;
+
+		k *= 2;
+	}
+
+	return (num);
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -13,11 +13,10 @@ What I learned from this project:
 * How to manipulate bits and use bitwise operators
 
 ---
-<!---
 ### [0. 0](./0-binary_to_uint.c)
 * A function that converts a binary number to an unsigned int.
 
-
+<!---
 ### [1. 1](./1-print_binary.c)
 * A function that prints the binary representation of a number.
 
