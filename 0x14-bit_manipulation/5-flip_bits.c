c4f4d490a4a781702f94e12880fde9d75a72b257
EstephaniaCalvoC <2177@holbertonschool.com>
2020-11-06 01:53:13 +0000

    Add 5-flip_bits.c function

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,26 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
- *
- * Return: 
+ * flip_bits - Calculate the number of bits to flip to get from
+ * one number to another.
+ * @n: Number 1.
+ * @m: Number 2.
+ * Return: The number of bits to flip to get from one number to another.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int c; /*Counter*/
+	short int bn, bm; /*Bit of n and m*/
 
+	for (c = 0; (n != 0) || (m != 0); n >>= 1, m >>= 1)
+	{
+		bn = n & 1;
+		bm = m & 1;
+
+		/*Count if the bits are differents*/
+		if (bn != bm)
+			c++;
+	}
+
+	return (c);
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -33,10 +33,8 @@ What I learned from this project:
 * A function that sets the value of a bit to 0 at a given index.
 
 
-<!---
 ### [5. 101](./5-flip_bits.c)
 * A function that returns the number of bits you would need to flip to get from one number to another.
--->
 ---
 
 ## Author
