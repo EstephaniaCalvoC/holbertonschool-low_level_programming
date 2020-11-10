773d082b1ed2e673634eabb93b46a4a2f6b59a59
EstephaniaCalvoC <2177@holbertonschool.com>
2020-11-09 20:47:24 -0500

    Fix loop error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -58,9 +58,10 @@ int main(int argc, char **argv)
 	/*Write*/
 	for (; n_chars > 0;)
 	{
-	        n_prints = write(file_to, buf, n_chars);
-	        if (n_prints == -1)
-			prt_error(99, argv[2]);
+		n_prints = write(file_to, buf, n_chars)
+			if (n_prints == -1)
+				prt_error(99, argv[2]);
+		n_chars = read(file_from, buf, 1024);
 	}
 	if (n_chars == -1)
 		prt_error(98, argv[1]);
