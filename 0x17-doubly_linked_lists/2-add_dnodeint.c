ab2c9d28a6533a22af122b9c5b56e4d246ebecc8
EstephaniaCalvoC <2177@holbertonschool.com>
2020-12-11 22:55:14 +0000

    Fix type error 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,14 +12,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	/*Create new node*/
 	new = malloc(sizeof(dlistint_t));
-	if (!new):
+	if (!new)
 		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 
-	if (!(*head)):
+	if (!(*head))
 		(*head)->prev = new;
 
 	*head = new;
