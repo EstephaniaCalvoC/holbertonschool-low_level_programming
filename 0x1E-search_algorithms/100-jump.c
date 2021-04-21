3da3b89b9e3f90762b6d76d759e5bd344718de79
EstephaniaCalvoC <2177@holbertonschool.com>
2021-04-21 15:04:36 -0500

    Fix second check print

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 	i -= jump;
 	for (j = 0; j <= jump && i < size; j++, i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, i);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
